63826a180f5cc740945a5c4990a98507b34e9147
amit169 <amit.lalloo786@gmail.com>
2022-04-28 16:46:08 +0400

    bug fix

--- js/quiz.js
+++ js/quiz.js
@@ -108,7 +108,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('../html/end.html')
+        return window.location.assign('../html/final.html')
     }
 
     questionCounter++
